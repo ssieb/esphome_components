ef570b12c13143fc4bda71d45a0da0b02bbe5b67
Samuel Sieb <samuel@sieb.net>
2022-08-24 23:26:01 -0700

    add demo component

---
+++ components/uart_demo/__init__.py
@@ -0,0 +1,75 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import automation
+from esphome.components import uart, binary_sensor, output, sensor, switch, text_sensor
+from esphome.const import CONF_ID, CONF_STATE, DEVICE_CLASS_VOLTAGE, ICON_FLASH, UNIT_VOLT
+
+MULTI_CONF = True
+DEPENDENCIES = ['uart']
+AUTO_LOAD = ['binary_sensor', 'output', 'sensor', 'switch', 'text_sensor']
+
+uart_demo_ns = cg.esphome_ns.namespace('uart_demo')
+
+UARTDemo = uart_demo_ns.class_('UARTDemo', cg.Component, uart.UARTDevice)
+UARTDemoBOutput = uart_demo_ns.class_("UARTDemoBOutput", output.BinaryOutput)
+UARTDemoFOutput = uart_demo_ns.class_("UARTDemoFOutput", output.FloatOutput)
+UARTDemoSwitch = uart_demo_ns.class_("UARTDemoSwitch", switch.Switch, cg.Component)
+
+CONF_THE_TEXT = "the_text"
+CONF_THE_SENSOR = "the_sensor"
+CONF_THE_BIN_OUTPUT = "the_bin_output"
+CONF_THE_FLT_OUTPUT = "the_flt_output"
+CONF_THE_BINSENSOR = "the_binsensor"
+CONF_THE_SWITCH = "the_switch"
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(UARTDemo),
+    cv.Optional(CONF_THE_TEXT): text_sensor.text_sensor_schema(text_sensor.TextSensor),
+    cv.Optional(CONF_THE_SENSOR): sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        icon=ICON_FLASH,
+        accuracy_decimals=1,
+        device_class=DEVICE_CLASS_VOLTAGE
+    ),
+    cv.Optional(CONF_THE_BIN_OUTPUT): output.BINARY_OUTPUT_SCHEMA.extend({cv.GenerateID(): cv.declare_id(UARTDemoBOutput)}),
+    cv.Optional(CONF_THE_FLT_OUTPUT): output.FLOAT_OUTPUT_SCHEMA.extend({cv.GenerateID(): cv.declare_id(UARTDemoFOutput)}),
+    cv.Optional(CONF_THE_BINSENSOR): binary_sensor.binary_sensor_schema(),
+    cv.Optional(CONF_THE_SWITCH): switch.SWITCH_SCHEMA.extend({cv.GenerateID(): cv.declare_id(UARTDemoSwitch)}),
+}).extend(uart.UART_DEVICE_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
+
+    if CONF_THE_TEXT in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_THE_TEXT])
+        cg.add(var.set_the_text(sens))
+
+    if CONF_THE_SENSOR in config:
+        sens = await sensor.new_sensor(config[CONF_THE_SENSOR])
+        cg.add(var.set_the_sensor(sens))
+
+    if CONF_THE_BIN_OUTPUT in config:
+        conf = config[CONF_THE_BIN_OUTPUT]
+        out = cg.new_Pvariable(conf[CONF_ID])
+        await output.register_output(out, conf)
+        cg.add(out.set_parent(var))
+
+    if CONF_THE_FLT_OUTPUT in config:
+        conf = config[CONF_THE_FLT_OUTPUT]
+        out = cg.new_Pvariable(conf[CONF_ID])
+        await output.register_output(out, conf)
+        cg.add(out.set_parent(var))
+
+    if CONF_THE_BINSENSOR in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_THE_BINSENSOR])
+        cg.add(var.set_the_binsensor(sens))
+
+    if CONF_THE_SWITCH in config:
+        conf = config[CONF_THE_SWITCH]
+        sw = cg.new_Pvariable(conf[CONF_ID])
+        await switch.register_switch(sw, conf)
+        cg.add(sw.set_parent(var))
+
---
+++ components/uart_demo/uart_demo.cpp
@@ -0,0 +1,73 @@
+#include "uart_demo.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace uart_demo {
+
+static const char *TAG = "uart_demo";
+
+void UARTDemo::setup() {
+}
+
+void UARTDemo::loop() {
+  while (this->available()) {
+    uint8_t c;
+    this->read_byte(&c);
+    this->handle_char_(c);
+  }
+}
+
+void UARTDemo::handle_char_(uint8_t c) {
+  if (c == '\r')
+    return;
+  if (c == '\n') {
+    std::string s(this->rx_message_.begin(), this->rx_message_.end());
+    if (this->the_text_ != nullptr)
+      this->the_text_->publish_state(s);
+    if (this->the_sensor_ != nullptr)
+      this->the_sensor_->publish_state(parse_number<float>(s).value_or(0));
+    if (this->the_binsensor_ != nullptr)
+      this->the_binsensor_->publish_state(s == "ON");
+    this->rx_message_.clear();
+    return;
+  }
+  this->rx_message_.push_back(c);
+}
+
+void UARTDemo::dump_config() {
+  LOG_TEXT_SENSOR("", "The Text Sensor", this->the_text_);
+  LOG_SENSOR("", "The Sensor", this->the_sensor_);
+}
+
+void UARTDemo::write_binary(bool state) {
+  this->write_str(ONOFF(state));
+}
+
+void UARTDemo::write_float(float state) {
+  this->write_str(to_string(state).c_str());
+}
+
+void UARTDemoBOutput::dump_config() {
+}
+
+void UARTDemoBOutput::write_state(bool state) {
+  this->parent_->write_binary(state);
+}
+
+void UARTDemoFOutput::dump_config() {
+}
+
+void UARTDemoFOutput::write_state(float state) {
+  this->parent_->write_float(state);
+}
+
+void UARTDemoSwitch::dump_config() {
+}
+
+void UARTDemoSwitch::write_state(bool state) {
+  this->parent_->write_binary(state);
+  this->publish_state(state);
+}
+
+}  // namespace uart_demo
+}  // namespace esphome
