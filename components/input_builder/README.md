e5600a15be16b5e8fc6a58923c9c9926d89be34a
Samuel Sieb <samuel@sieb.net>
2022-09-11 18:07:17 -0700

    add input_builder

---
+++ components/input_builder/README.md
@@ -0,0 +1,29 @@
+# input_builder component
+
+This component collects key presses from a `key_provider` like the `matrix_keypad` or `wiegand` components.
+
+You need at least one of the `end_keys` or `max_length` parameters.  The rest are optional.
+
+Example:
+```yaml
+input_builder:
+  - id: pin_reader
+    source_id: mykeypad
+    min_length: 4
+    max_length: 4
+    end_keys: "#"
+    end_key_required: true   # default is false
+    back_keys: "*"
+    clear_keys: "C"
+    timeout: 5s
+    allowed_keys: "0123456789"  # if not included, then any otherwise unused keys will be allowed
+    on_progress:
+      - logger.log: 
+          format: "input progress: '%s'"
+          args: [ 'x.c_str()' ]
+    on_result:
+      - logger.log: 
+          format: "input result: '%s'"
+          args: [ 'x.c_str()' ]
+```
+
---
+++ components/input_builder/input_builder.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "esphome/components/key_provider/key_provider.h"
+#include "esphome/core/automation.h"
+
+namespace esphome {
+namespace input_builder {
+
+class InputBuilder : public Component {
+ public:
+  InputBuilder();
+  void loop() override;
+  void dump_config() override;
+  void set_provider(key_provider::KeyProvider *provider);
+  void set_min_length(int min_length) { this->min_length_ = min_length; };
+  void set_max_length(int max_length) { this->max_length_ = max_length; };
+  void set_end_keys(std::string end_keys) { this->end_keys_ = end_keys; };
+  void set_end_key_required(bool end_key_required) { this->end_key_required_ = end_key_required; };
+  void set_back_keys(std::string back_keys) { this->back_keys_ = back_keys; };
+  void set_clear_keys(std::string clear_keys) { this->clear_keys_ = clear_keys; };
+  void set_allowed_keys(std::string allowed_keys) { this->allowed_keys_ = allowed_keys; };
+  Trigger<std::string> *get_progress_trigger() const { return this->progress_trigger_; };
+  Trigger<std::string> *get_result_trigger() const { return this->result_trigger_; };
+  void set_timeout(int timeout) { this->timeout_ = timeout; };
+
+ protected:
+  void key_pressed_(uint8_t key);
+
+  int min_length_{0};
+  int max_length_{0};
+  std::string end_keys_;
+  bool end_key_required_{false};
+  std::string back_keys_;
+  std::string clear_keys_;
+  std::string allowed_keys_;
+  std::string result_;
+  Trigger<std::string> *progress_trigger_;
+  Trigger<std::string> *result_trigger_;
+  uint32_t last_key_time_;
+  uint32_t timeout_{0};
+};
+
+}  // namespace input_builder
+}  // namespace esphome
+
