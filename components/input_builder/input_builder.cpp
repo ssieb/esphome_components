7af27b82bd13e163e79912b77c2d6712de8e0478
NP v/d Spek <nielsnl68@gmail.com>
2022-10-25 16:49:21 +0200

    forgot to change some field names

--- components/input_builder/input_builder.cpp
+++ components/input_builder/input_builder.cpp
@@ -46,13 +46,13 @@ void InputBuilder::set_provider(key_provider::KeyProvider *provider) {
   
 bool InputBuilder::can_handle_(uint8_t key) {
   if (!this->is_started_ ) 
-    this->is_started_  = this->back_keys_.find(key) != std::string::npos);
+    this->is_started_  = this->start_keys_.find(key) != std::string::npos);
   return this->is_started_;
 }
   
 void InputBuilder::key_pressed_(uint8_t key) {
   this->last_key_time_ = millis();
-  if (!this->end_keys_.empty() && !this->can_handle_(key)) 
+  if (!this->start_keys_.empty() && !this->can_handle_(key)) 
     return;
   if (this->back_keys_.find(key) != std::string::npos) {
     if (!this->result_.empty()) {
