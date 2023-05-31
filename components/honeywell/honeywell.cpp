77ce298e55accb97ed06516bac9f031a9ba7230c
Samuel Sieb <samuel@sieb.net>
2023-05-31 12:09:00 -0700

    correct message format

--- components/honeywell/honeywell.cpp
+++ components/honeywell/honeywell.cpp
@@ -19,16 +19,16 @@ void HoneyWell::loop() {
     std::string s(this->rx_message_.begin(), this->rx_message_.end());
     this->rx_message_.clear();
     ESP_LOGD(TAG, "received message: %s", s.c_str());
-    // KEYS[02] 0x01 0x0a
-    if ((s.length() < 8) || (s.substr(0, 6) != "KEYS[0")) {
+    // KEYS_XX[02] 0x01 0x0a
+    if ((s.length() < 11) || (s.substr(0, 5) != "KEYS_")) {
       ESP_LOGD(TAG, "unknown message");
       continue;
     }
-    int count = s[6] - '0';
+    int count = s[9] - '0';
     for (int i = 0; i < count; i++) {
-      if (s.length() >= 8 + (i + 1) * 5) {
+      if (s.length() >= 11 + (i + 1) * 5) {
         uint8_t key;
-        parse_hex(s.c_str() + 11 + i * 5, &key, 2);
+        parse_hex(s.c_str() + 14 + i * 5, &key, 2);
         if (key <= 9)
           key += '0';
         else if (key == 0xa)
