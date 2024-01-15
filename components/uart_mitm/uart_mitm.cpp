88cb2f4b704ebb3638b67b1ce7048fe00f977406
RoganDawes <rogan@dawes.za.net>
2024-01-15 09:13:34 +0200

    Reduce latency in full-duplex scenarios
    It's a subtle difference, but it reduces per-character latency for a full duplex stream. Otherwise one direction is held up while the other direction is busy.

--- components/uart_mitm/uart_mitm.cpp
+++ components/uart_mitm/uart_mitm.cpp
@@ -8,13 +8,15 @@ static const char *const TAG = "uart_mitm";
 
 void UARTMITM::loop() {
   uint8_t c;
-  while (this->uart1_->available()) {
-    this->uart1_->read_byte(&c);
-    this->uart2_->write_byte(c);
-  }
-  while (this->uart2_->available()) {
-    this->uart2_->read_byte(&c);
-    this->uart1_->write_byte(c);
+  while (this->uart1_->available() || this->uart2_->available()) {
+    if (this->uart1_->available()) {
+      this->uart1_->read_byte(&c);
+      this->uart2_->write_byte(c);
+    }
+    if (this->uart2_->available()) {
+      this->uart2_->read_byte(&c);
+      this->uart1_->write_byte(c);
+    }
   }
 }
 
