3c3e46b017698c60614e7a51b746e578d5d500a7
Samuel Sieb <samuel@sieb.net>
2025-01-22 20:54:02 -0800

    handle lines ending with only returns

--- components/serial/text_sensor/serial_text_sensor.cpp
+++ components/serial/text_sensor/serial_text_sensor.cpp
@@ -15,15 +15,25 @@ void SerialTextSensor::loop() {
 }
 
 void SerialTextSensor::handle_char_(uint8_t c) {
-  if (c == '\r')
-    return;
-  if (c == '\n') {
-    std::string s(this->rx_message_.begin(), this->rx_message_.end());
-    this->publish_state(s);
-    this->rx_message_.clear();
+  if (c == '\r') {
+    this->have_return_ = true;
+    this->publish_();
     return;
   }
+  if (c == '\n') {
+    if (!have_return_)
+      this->publish_();
+    this->have_return_ = false;
+    return;
+  }
+  this->have_return_ = false;
   this->rx_message_.push_back(c);
+}
+
+void SerialTextSensor::publish_() {
+  std::string s(this->rx_message_.begin(), this->rx_message_.end());
+  this->publish_state(s);
+  this->rx_message_.clear();
 }
 
 void SerialTextSensor::dump_config() { LOG_TEXT_SENSOR("", "Serial Sensor", this); }
